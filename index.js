1b4b34f2a72604715f2639d4f3d343aec318d74d
teck7 <teckyap@hotmail.com.au>
2017-07-08 19:59:14 +1000

    add lodash / user json / dynamic user routes

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello ExpressJs!!!!')
+var fs = require('fs')
+var _ = require('lodash')
+var users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
+  if (err) throw err
+
+  JSON.parse(data).forEach(function (user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  })
+
 })
 
 app.get('/welcome', function(req, res) {
   res.send('Welcome to Express')
+})
+
+app.get('/', function (req, res) {
+  var buffer = ''
+
+  users.forEach(function(user) {
+    buffer += '<a href="/' + user.name.full + '">' + user.name.full + '</a><br>'
+  })
+  res.send(buffer)
+})
+
+app.get('/:username', function (req, res) {
+  var username = req.params.username
+  res.send(username)
 })
 
 var server = app.listen(3000, function() {
